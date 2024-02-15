ed75e6d45d70058e47ed751c3bf7c9ff2b74203f
BK <trankhang1068@gmail.com>
2024-02-15 16:49:20 +0700

    complete

---
+++ CMakeFiles/CMakeHello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeHello.dir/main.c.o"
+  "CMakeFiles/CMakeHello.dir/main.c.o.d"
+  "CMakeHello"
+  "CMakeHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMakeHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc , char **argv)
+{
+	printf("hello Cmake \n");
+	return 0;
+}
